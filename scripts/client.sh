d5c157cd44a3b1c5a361b3bc8f9a5d6a10005442
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-16 19:42:03 -0500

    Updated client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
 sudo su 
-cd /home/uab2005/dom-tenant-service/src/build
-./multicast_client -t 10 -i 1
+cd /home/uab2005/dom-tenant-service/src
+./build/multicast_client -a mac -t 10 -i 0 -s test
+./build/multicast_client -a messages -t 10 -i 0 -s test
