1d8ce9d1c0ef244d05a91f76efb038b42e32ff79
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-19 14:18:42 -0500

    Adding useful library script

---
+++ utils.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+gjn() {
+    [ -z $1 ] && INSTANCE="recipient-dev" || INSTANCE="$1"
+    gcloud compute ssh --zone "us-east4-c" \
+                        "${INSTANCE}" \
+                        --project "multicast1" \
+                        --command="sudo journalctl -u google-startup-scripts.service"
+}
+
+glog() {
+    [ -z $1 ] && INSTANCE="recipient-dev" || INSTANCE="$1"
+    gjn ${INSTANCE} > LOG.TXT
+}
