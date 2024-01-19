38e897de5a8f8b9d753fd5e50b9057d0d6bcca93
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-19 18:15:24 -0500

    Updated utils.sh

--- utils.sh
+++ utils.sh
@@ -20,13 +20,17 @@ glogf() {
 }
 
 groot() {
-    [ -z $1 ] && INSTANCE="recipient-dev" || INSTANCE="$1"
-    [ -z $2 ] && COMMAND="cd /home/uab2005" || COMMAND="$2"
+    [ -z "$1" ] && INSTANCE="recipient-dev" || INSTANCE="$1"
+    [ -z "$2" ] && COMMAND="cd /home/uab2005" || COMMAND="$2"
     gcloud compute ssh "root@${INSTANCE}" --zone "us-east4-c" --project "multicast1" -- "${COMMAND}; bash"
 }
 
 gcli() {
     groot client0000 "cd /home/uab2005/dom-tenant-service/src"
+}
+
+gprox() {
+    groot proxy0000
 }
 
 grec() {
