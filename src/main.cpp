a31698fded64f1f22d05e213d93b1d2daec2accb
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-19 14:07:34 -0500

    Added correct necessary libs for DPDK hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright [2023] NYU
 
+#include <rte_eal.h>
+#include <rte_cycles.h>
+#include <rte_errno.h>
+#include <rte_ethdev.h>
+
 #include <cstdlib>
+#include <cstdio>
 
 int main(int argc, char *argv[]) {
     int ret = rte_eal_init(argc, argv);
@@ -9,5 +15,6 @@ int main(int argc, char *argv[]) {
         rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
     }
 
+    printf("Number of ETHERNET DEVICES: %d", rte_eth_dev_count_avail());
     return 0;
 }
