566daacc8d1f21a9bccdb8bebbda33577bd177de
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-19 14:39:18 -0500

    Updated utils.sh

--- utils.sh
+++ utils.sh
@@ -19,10 +19,6 @@ glogf() {
 }
 
 groot() {
-    local COMMAND="sudo su; cd /home/uab2005"
     [ -z $1 ] && INSTANCE="recipient-dev" || INSTANCE="$1"
-    gcloud compute ssh --zone "us-east4-c" \
-                        "${INSTANCE}" \
-                        --project "multicast1" \
-                        --command="bash -i -c env" -- ${COMMAND}
+    gcloud compute ssh "root@${INSTANCE}" --zone "us-east4-c" --project "multicast1" -- "cd /home/uab2005; bash"
 }
