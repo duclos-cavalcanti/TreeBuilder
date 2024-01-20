032a6bdcd1d8ee8788b7c7d24dec8f7b1a49fc09
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-20 18:02:38 -0500

    Added package action and restructure

--- scripts/dts.sh
+++ scripts/dts.sh
@@ -6,7 +6,7 @@ err() {
 }
 
 usage() {
-    local string=" USAGE: ${0} <action>\nactions: create, deploy, delete"
+    local string=" USAGE: ${0} <action>"
     err "${string}"
 }
 
@@ -14,26 +14,34 @@ main() {
     local action="$1"
     local dir=$(basename $(pwd))
     if [ -d "./dom-tenant-service" ]; then 
-        pushd dom-tenant-service
-            pushd gcp-deploy
-                case $action in
-                    create)
-                        python3 run.py create-template -r 10 -p 3 -b 2 -c 1 -rm dpdk -dm standard -conf 0
-                        ;;
-
-                    deploy)
-                        python3 run.py deploy-stack
-                        ;;
-
-                    delete)
-                        python3 run.py delete-stack
-                        ;;
-                    *)
-                        err "INVALID OPTION: ${action}"
-                        ;;
-                esac
-            popd
-        popd
+        case $action in
+            create)
+                pushd dom-tenant-service; pushd gcp-deploy
+                python3 run.py create-template -r 10 -p 3 -b 2 -c 1 -rm dpdk -dm standard -conf 0
+                popd; popd
+                ;;
+        
+            deploy)
+                pushd dom-tenant-service; pushd gcp-deploy
+                python3 run.py deploy-stack
+                popd; popd
+                ;;
+        
+            package)
+                rm -f dts.tar.gz
+                tar -czf dts.tar.gz  --exclude .git -C ./dom-tenant-service .
+                gcloud storage cp dts.tar.gz gs://cdm-templates-nyu-systems-multicast/bundled_proj.tar.gz
+                ;;
+        
+            delete)
+                pushd dom-tenant-service; pushd gcp-deploy
+                python3 run.py delete-stack
+                popd; popd
+                ;;
+            *)
+                err "INVALID OPTION: ${action}"
+                ;;
+        esac
     else 
         err "Incorrect base directory: ${dir}"
     fi
