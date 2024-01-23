fbd48cb9c2eee55aa1c5fcb2a21092d8a44517d8
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-22 22:18:09 -0500

    Updated script

--- scripts/dts.sh
+++ scripts/dts.sh
@@ -16,15 +16,21 @@ main() {
     if [ -d "./module" ]; then 
         case $action in
             create)
-                pushd module; pushd gcp-deploy
-                python3 run.py create-template -r 10 -p 3 -b 2 -c 1 -rm dpdk -dm standard -conf 0
-                popd; popd
+                large="python3 run.py create-template -r 100 -p 11 -b 10 -c 1 -rm dpdk -dm standard -conf 0"
+                medium="python3 run.py create-template -r 10 -p 3 -b 2 -c 1 -rm dpdk -dm standard -conf 0"
+                simple="python3 run.py create-template -r 1 -p 1 -b 1 -c 1 -rm dpdk -dm standard -conf 0"
+                pushd module/gcp-deploy
+                echo ${simple}
+                ${simple}
+                popd
                 ;;
         
             deploy)
-                pushd module; pushd gcp-deploy
-                python3 run.py deploy-stack
-                popd; popd
+                command="python3 run.py deploy-stack"
+                pushd module/gcp-deploy
+                echo ${command}
+                ${command}
+                popd
                 ;;
         
             package)
@@ -34,8 +40,10 @@ main() {
                 ;;
         
             delete)
+                command="python3 run.py delete-stack"
                 pushd module; pushd gcp-deploy
-                python3 run.py delete-stack
+                echo ${command}
+                ${command}
                 popd; popd
                 ;;
             *)
