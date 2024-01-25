6254d3e6ecf19325bac062d9e5e7e93f38cc0b7c
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-01-25 12:57:40 -0500

    Cleanup

--- utils.sh
+++ utils.sh
@@ -10,19 +10,6 @@ check() {
 check fzf 
 check gcloud
 
-glog() {
-    local INSTANCE=$(gcloud compute instances list | tail -n +2 | awk '{print $1}' |  fzf --height 40% --reverse)
-    gcloud compute ssh --zone "us-east4-c" \
-                        "${INSTANCE}" \
-                        --project "multicast1" \
-                        --command="sudo journalctl -u google-startup-scripts.service" | less
-}
-
-gboot() {
-    [ -z "$1" ] && INSTANCE="base-dev" || INSTANCE="$1"
-    gcloud compute instances reset ${INSTANCE} --zone="us-east4-c"
-}
-
 groot() {
     [ -z "$1" ] && INSTANCE="base-dev" || INSTANCE="$1"
     [ -z "$2" ] && COMMAND="cd /home/uab2005" || COMMAND="$2"
@@ -41,6 +28,15 @@ grec() {
     groot recipient0000 "cd /home/uab2005/dom-tenant-service/src; sudo journalctl -f -u google-startup-scripts.service"
 }
 
+glog() {
+    local INSTANCE=$(gcloud compute instances list | tail -n +2 | awk '{print $1}' |  fzf --height 40% --reverse)
+    gcloud compute ssh --zone "us-east4-c" \
+                        "${INSTANCE}" \
+                        --project "multicast1" \
+                        --command="sudo journalctl f -u google-startup-scripts.service" | less
+}
+
+
 gsh() {
     local INSTANCE=${1}
     local COMMAND=${2}
@@ -48,7 +44,9 @@ gsh() {
 }
 
 gsel() {
-    local INSTANCE=$(gcloud compute instances list | tail -n +2 | awk '{print $1}' |  fzf --height 40% --reverse)
+    local INSTANCE=
+    local COMMAND=
+    INSTANCE=$(gcloud compute instances list | tail -n +2 | awk '{print $1}' |  fzf --height 40% --reverse)
     [ -z "$1" ] && COMMAND="cd /home/uab2005" || COMMAND="$1"
     gsh ${INSTANCE} ${COMMAND}
 }
