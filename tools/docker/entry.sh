3821b55773f7f90efb8f6b177b57208d67aceb79
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-02-05 23:29:33 -0500

    Updated docker entries again

--- tools/docker/entry.sh
+++ tools/docker/entry.sh
@@ -1,16 +1,14 @@
 #!/bin/bash 
 
-# dev related packages
+# non-standardn dev related packages
 {
-    # installing neovim
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
     tar xzvf nvim-linux64.tar.gz
     cp ./nvim-linux64/bin/nvim /usr/local/bin
     cp -r ./nvim-linux64/share/* /usr/local/share
     cp -r ./nvim-linux64/lib/* /usr/local/lib
     
-    # installing C/C++ LSP, man pages and more
-    apt-get install clangd man-db ripgrep -yq
+    apt-get install ripgrep -yq
     unminimize
 } &>> LOG.TXT
 
