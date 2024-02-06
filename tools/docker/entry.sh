e9f1188327c858af0b2a0525c6784bd2256d1044
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-02-05 20:59:53 -0500

    Updated docker relevant packages and startup

--- tools/docker/entry.sh
+++ tools/docker/entry.sh
@@ -1,13 +1,17 @@
 #!/bin/bash 
 
+# dev related packages
 {
+    # installing neovim
     wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
     tar xzvf nvim-linux64.tar.gz
     cp ./nvim-linux64/bin/nvim /usr/local/bin
     cp -r ./nvim-linux64/share/* /usr/local/share
     cp -r ./nvim-linux64/lib/* /usr/local/lib
     
-    apt-get install clangd -yq
+    # installing C/C++ LSP, man pages and more
+    apt-get install clangd man-db ripgrep -yq
+    unminimize
 } &>> LOG.TXT
 
 cd /root/work/module
