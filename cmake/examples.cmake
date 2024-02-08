d5d33a6bc811f4782aff0c8d311256ee846c4da7
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-02-07 19:06:36 -0500

    Added zeromq as a library to the project

--- cmake/examples.cmake
+++ cmake/examples.cmake
@@ -6,26 +6,29 @@ foreach(NAME IN LISTS DIRS)
     if(IS_DIRECTORY ${EXAMPLES_DIR}/${NAME})
         file(GLOB SOURCES "${EXAMPLES_DIR}/${NAME}/*.c")
 
+        set(EXAMPLE_TARGET          "x_${NAME}")
+        set(EXAMPLE_SOURCES         "${SOURCES}")
+        set(EXAMPLE_INCLUDES        "${EXAMPLES_DIR}/${NAME}/" "${LIBRARY_INCLUDES}")
+        set(EXAMPLE_LIBRARIES       "${LIBRARIES}")
+
         if(SOURCES STREQUAL "")
             file(GLOB SOURCES "${EXAMPLES_DIR}/${NAME}/*.cpp")
+            set(EXAMPLE_SOURCES         "${SOURCES}")
+            set(EXAMPLE_INCLUDES        "${EXAMPLES_DIR}/${NAME}/" "${LIBRARY_INCLUDES}" "${ZEROMQ_INCLUDE_DIR}")
+            set(EXAMPLE_LIBRARIES       "${LIBRARIES}" "${ZEROMQ_LINK_LIBRARIES}")
         endif()
 
         if(SOURCES STREQUAL "")
             message(FATAL_ERROR "No C or C++ source files found: ${EXAMPLES_DIR}/${NAME}")
         endif()
 
-        set(EXAMPLE_TARGET "example_${NAME}")
-        set(EXAMPLE_SOURCES ${SOURCES})
-        set(EXAMPLE_INCLUDES "${EXAMPLES_DIR}/${NAME}/" ${DPDK_INCLUDE_DIRS} ${LIBURING_INCLUDE_DIRS})
-
-        add_executable(${EXAMPLE_TARGET} EXCLUDE_FROM_ALL ${EXAMPLE_SOURCES})
-        include_directories(${EXAMPLE_INCLUDES})
-        target_link_libraries(${EXAMPLE_TARGET} PRIVATE ${DPDK_LIBRARIES} ${LIBURING_LIBRARIES})
-        target_include_directories(${EXAMPLE_TARGET} PRIVATE ${DPDK_INCLUDE_DIRS} ${LIBURING_INCLUDE_DIRS})
+        add_executable(${EXAMPLE_TARGET}                EXCLUDE_FROM_ALL ${EXAMPLE_SOURCES})
+        target_include_directories(${EXAMPLE_TARGET}    PRIVATE ${EXAMPLE_INCLUDES})
+        target_link_libraries(${EXAMPLE_TARGET}         PRIVATE ${EXAMPLE_LIBRARIES})
 
         list(APPEND ALL_EXAMPLE_TARGETS ${EXAMPLE_TARGET})
     endif()
 endforeach()
 
 add_custom_target(examples DEPENDS ${ALL_EXAMPLE_TARGETS})
-message(STATUS "examples: ${ALL_EXAMPLE_TARGETS}")
+message(STATUS "EXAMPLES: ${ALL_EXAMPLE_TARGETS}")
---
+++ cmake/zeromq.cmake
@@ -0,0 +1,18 @@
+include(ExternalProject)
+set(ZEROMQ_INSTALL_DIR ${CMAKE_BINARY_DIR}/lib/ZeroMQ)
+
+ExternalProject_Add(zeroMQ
+    GIT_REPOSITORY https://github.com/zeromq/libzmq.git
+    GIT_TAG master
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${ZEROMQ_INSTALL_DIR}
+        -DWITH_PERF_TOOL=OFF
+        -DZMQ_BUILD_TESTS=OFF
+        -DENABLE_CPACK=OFF
+        -DBUILD_SHARED_LIBS=ON 
+    INSTALL_DIR ${ZEROMQ_INSTALL_DIR}
+)
+
+set(ZEROMQ_INCLUDE_DIR      "${ZEROMQ_INSTALL_DIR}/include")
+set(ZEROMQ_LIBRARY_DIRS     "${ZEROMQ_INSTALL_DIR}/lib/")
+set(ZEROMQ_LINK_LIBRARIES   "${ZEROMQ_INSTALL_DIR}/lib/libzmq${CMAKE_SHARED_LIBRARY_SUFFIX}")
