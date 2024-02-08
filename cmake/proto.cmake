d4affe7bb676b619f594125521a098296bbae283
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-02-07 19:41:30 -0500

    Included proto in the project

---
+++ cmake/proto.cmake
@@ -0,0 +1,10 @@
+set(PROTO_BIN   protobin)
+set(PROTO_FILES file.proto)
+
+message(STATUS "PROTOBUF_INCLUDE_DIRS: "    ${PROTOBUF_INCLUDE_DIRS})
+message(STATUS "PROTOBUF_LIBRARIES: "       ${PROTOBUF_LIBRARIES})
+
+# protobuf_generate_cpp(PROTO_SRCS PROTO_HDRS ${PROTO_FILES})
+# add_executable(${PROTO_BIN} ${PROTO_SRCS} ${PROTO_HDRS})
+# include_directories(${PROTOBUF_INCLUDE_DIRS})
+# target_link_libraries(${PROTO_BIN} ${PROTOBUF_LIBRARIES})
