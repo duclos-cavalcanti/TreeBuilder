77a2acdd0397cec1fecc5f2dc60d6d2f0eedc427
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-02-23 19:03:10 -0500

    Adapted CMake to use proto and the local zmq libraries correctly

--- cmake/examples.cmake
+++ cmake/examples.cmake
@@ -1,6 +1,7 @@
 set(EXAMPLES_DIR ${PROJECT_SOURCE_DIR}/examples)
 set(ALL_EXAMPLE_TARGETS "")
 
+
 file(GLOB DIRS RELATIVE ${EXAMPLES_DIR} ${EXAMPLES_DIR}/*)
 foreach(NAME IN LISTS DIRS)
     if(IS_DIRECTORY ${EXAMPLES_DIR}/${NAME})
@@ -13,16 +14,20 @@ foreach(NAME IN LISTS DIRS)
 
         if(SOURCES STREQUAL "")
             file(GLOB SOURCES "${EXAMPLES_DIR}/${NAME}/*.cpp")
-            set(EXAMPLE_SOURCES         "${SOURCES}")
-            set(EXAMPLE_INCLUDES        "${EXAMPLES_DIR}/${NAME}/" "${LIBRARY_INCLUDES}" "${CPPZMQ_INCLUDE_DIRS}")
-            set(EXAMPLE_LIBRARIES       "${LIBRARIES}" "${CPPZMQ_LINK_LIBRARIES}")
+            set(EXAMPLE_SOURCES         "${SOURCES}" "${PROTO_SRCS}" "${PROTO_HDRS}")
+            set(EXAMPLE_INCLUDES        "${EXAMPLES_DIR}/${NAME}/" "${ZEROMQ_INCLUDE_DIRS}" "${CPPZMQ_INCLUDE_DIRS}" "${PROTOBUF_INCLUDE_DIRS}" "${CMAKE_CURRENT_BINARY_DIR}")
+            set(EXAMPLE_LIBRARY_DIRS    "${CPPZMQ_LIBRARY_DIRS}")
+            set(EXAMPLE_LIBRARIES       "${LIBZMQ_LINK_LIBRARIES}" "${PROTOBUF_LIBRARIES}")
 
             add_executable(${EXAMPLE_TARGET}                EXCLUDE_FROM_ALL ${EXAMPLE_SOURCES})
             target_compile_definitions(${EXAMPLE_TARGET}    PRIVATE ZMQ_BUILD_DRAFT_API=1)
             target_include_directories(${EXAMPLE_TARGET}    PRIVATE ${EXAMPLE_INCLUDES})
+            target_link_directories(${EXAMPLE_TARGET}       PRIVATE ${EXAMPLE_LIBRARY_DIRS})
             target_link_libraries(${EXAMPLE_TARGET}         PRIVATE ${EXAMPLE_LIBRARIES})
-            # message(STATUS "CPPZMQ_INCLUDE_DIRS: ${CPPZMQ_INCLUDE_DIRS}")
-            # message(STATUS "CPPZMQ_LINK_LIBRARIES: ${CPPZMQ_LINK_LIBRARIES}")
+
+            # message(STATUS "SRCS: ${EXAMPLE_SOURCES}")
+            # message(STATUS "INCLUDE_DIRS: ${EXAMPLE_INCLUDES}")
+            # message(STATUS "LINK_LIBRARIES: ${EXAMPLE_LIBRARIES}")
         else()
             add_executable(${EXAMPLE_TARGET}                EXCLUDE_FROM_ALL ${EXAMPLE_SOURCES})
             target_include_directories(${EXAMPLE_TARGET}    PRIVATE ${EXAMPLE_INCLUDES})
--- cmake/proto.cmake
+++ cmake/proto.cmake
@@ -1,10 +1,12 @@
+pkg_search_module(PROTOBUF REQUIRED protobuf)
+
 set(PROTO_BIN   protobin)
-set(PROTO_FILES file.proto)
+set(PROTO_FILES ${PROJECT_SOURCE_DIR}/lib/proto/message.proto)
+set(PROTO_LIBRARY_DIR ${PROJECT_SOURCE_DIR}/lib/proto)
 
-# message(STATUS "PROTOBUF_INCLUDE_DIRS: "    ${PROTOBUF_INCLUDE_DIRS})
-# message(STATUS "PROTOBUF_LIBRARIES: "       ${PROTOBUF_LIBRARIES})
+protobuf_generate_cpp(PROTO_SRCS PROTO_HDRS ${PROTO_FILES})
 
-# protobuf_generate_cpp(PROTO_SRCS PROTO_HDRS ${PROTO_FILES})
-# add_executable(${PROTO_BIN} ${PROTO_SRCS} ${PROTO_HDRS})
-# include_directories(${PROTOBUF_INCLUDE_DIRS})
-# target_link_libraries(${PROTO_BIN} ${PROTOBUF_LIBRARIES})
+# message(STATUS "PROTO_SRCS: ${PROTO_SRCS}")
+# message(STATUS "PROTO_HDRS: ${PROTO_HDRS}")
+# message(STATUS "PROTOBUF_INCLUDE_DIRS = ${PROTOBUF_INCLUDE_DIRS}")
+# message(STATUS "PROTOBUF_LIBRARIES = ${PROTOBUF_LIBRARIES}")
