544c59ffab6c05b35a1a21107cd852f1aab4ae80
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-03-17 21:23:34 -0400

    Update docker entry script to be a simple change dir and bash

--- tools/docker/entry.sh
+++ tools/docker/entry.sh
@@ -1,16 +1,4 @@
 #!/bin/bash 
 
-# non-standardn dev related packages
-{
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-    tar xzvf nvim-linux64.tar.gz
-    cp ./nvim-linux64/bin/nvim /usr/local/bin
-    cp -r ./nvim-linux64/share/* /usr/local/share
-    cp -r ./nvim-linux64/lib/* /usr/local/lib
-    
-    apt-get install ripgrep -yq
-    unminimize
-} &>> LOG.TXT
-
-cd /root/work/module
+cd /root/work/jasper
 bash
