091593771304bf55e357e05f7fa91a632d317dc0
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-03-28 17:02:50 -0400

    Created simple GCP launch script

---
+++ deploy/instance.py
@@ -0,0 +1,92 @@
+import yaml
+import datetime
+
+def _timestamp():
+    cur = datetime.datetime.now()
+    cur = cur.strftime("%H:%M:%S")
+    cur = cur[:8]
+    cur = cur.replace(":", "")
+    return cur
+
+class Instance():
+    def __init__(self):
+        self.zone = 'us-east4-c'
+        self.disks      = {
+            'autoDelete': True,
+            'boot': True,
+            'deviceName': 'boot',
+            'initializeParams': {
+                'sourceImage': 'projects/multicast1/global/images'
+            },
+            'type': 'PERSISTENT'
+        }
+        self.machineType = "zones/us-east4-c/machineTypes"
+        self.metadata = {
+            'items': [
+                {   
+                    'key': 'startup-script', 
+                    'value': 'ls /'
+                },
+            ]
+        }
+        self.networkInterfaces = [
+            {
+                'accessConfigs': [
+                    {'networkTier': 'PREMIUM', 'type': 'ONE_TO_ONE_NAT'}
+                ],
+                'network': 'projects/multicast1/global/networks/multicast-management',
+                'networkIP': '10.1.1.255',
+                'nicType': 'GVNIC',
+                'stackType': 'IPV4_ONLY',
+                'subnetwork': 'projects/multicast1/regions/us-east4/subnetworks/multicast-management'
+            },
+            {
+                'network': 'projects/multicast1/global/networks/multicast-service',
+                'networkIP': '10.0.1.255',
+                'nicType': 'GVNIC',
+                'stackType': 'IPV4_ONLY',
+                'subnetwork': 'projects/multicast1/regions/us-east4/subnetworks/multicast-service'
+            }
+        ]
+        self.serviceAccounts = [
+            {
+                'email': 'multicast-service-vm@multicast1.iam.gserviceaccount.com',
+                'scopes': ['https://www.googleapis.com/auth/cloud-platform']
+            }
+        ]
+
+    def set_ips(self, ip:str):
+        raise(NotImplementedError)
+
+    def set_disk(self, disk="multicast-ebpf-zmq-grub-disk"):
+        src = self.disks["initializeParams"]["sourceImage"]
+        src = f"{src}/{disk}"
+        self.disks["initializeParams"]["sourceImage"] = src
+
+    def set_machine(self, machine:str="c2d-highcpu-8"):
+        self.machineType = f"{self.machineType}/{machine}"
+
+    def set_zone(self, zone="us-east4-c"):
+        self.zone = f"{zone}"
+
+    def generate(self, name):
+        ts = _timestamp()
+        data = {
+            'resources': [
+                {
+                    'name': f"{name}",
+                    'properties': {
+                        'disks': [self.disks],
+                        'machineType': self.machineType,
+                        'metadata': self.metadata,
+                        'networkInterfaces': self.networkInterfaces,
+                        'serviceAccounts': self.serviceAccounts,
+                        'zone': self.zone
+                    },
+                    'type': 'compute.v1.instance'
+                }
+            ]
+        }
+        ret = f"stack_{ts}.yaml"
+        with open(ret, "w") as stream: yaml.dump(data, stream)
+        return ret
--- deploy/run.py
+++ deploy/run.py
@@ -1,93 +1,43 @@
-import os
-import sys
 import subprocess
 import argparse
-import yaml
 
-STACK   = "duclos-dev"
-BUCKET  = "duclos-dev-storage"
-CONFIG  = "./build/instance.yaml"
+import instance
 
-DEPLOY  =   f"gcloud deployment-manager -q deployments create {STACK} --config"
-DELETE  =   f"gcloud deployment-manager -q deployments delete {STACK}"
-COPY    =   f"gcloud storage cp"
-
-def upload(src, dst, origin=f"{BUCKET}"):
-    command=f"{COPY} {src} gs://{origin}/{dst}"
+def store(src, dst, origin=f"duclos-dev-storage"):
+    command=f"gcloud storage cp {src} gs://{origin}/{dst}"
     subprocess.run(command, shell=True)
     return
 
-def pkg():
-    folder="dom-tenant-service"
-    package = "bundle.tar.gz"
-    if os.path.exists(package):
-        os.remove(package)
+def template(name="vanilla"):
+    i = instance.Instance()
+    i.set_disk(disk="multicast-ebpf-zmq-grub-disk")
+    i.set_machine(machine="c2d-highcpu-8")
+    i.set_zone(zone="us-east4-c")
+    return i.generate(name)
 
-    command = f"tar -czf ./build/{package} --exclude .git -C ./{folder}/ ."
-    subprocess.run(command, shell=True)
-
-    upload(f"./build/{package}", package)
-    upload("./assets/startup/base.sh", "base.sh")
-    upload("./assets/startup/client.sh", "client.sh")
-    upload("./assets/startup/proxy.sh", "proxy.sh")
-    upload("./assets/startup/recipient.sh", "recipient.sh")
-    return
-
-def template():
-    def load_yaml(n):
-        with open(n, "r") as stream:
-            return yaml.safe_load(stream)
-    
-    def write_yaml(n, data):
-        with open(n, "w") as stream:
-            yaml.dump(data, stream)
-
-    base = "./assets/base.yaml"
-    instance = CONFIG
-    instances = [ "base-dev", "client-dev", "proxy-dev", "recipient-dev"]
-
-    data = load_yaml(base)
-    for i in range(len(data["resources"])):
-        name = data["resources"][i]["name"] 
-
-        if name not in instances:
-            print(f"Unexpected yaml file format, name of resource is {name}, not found in instances array!")
-            sys.exit(1)
-
-        script = instances[instances.index(name)][:-len("-dev")]
-        startup = f"""
-            #!/bin/bash
-            pushd /home/uab2005/
-            gcloud storage cp gs://{BUCKET}/{script}.sh ./{script}.sh 
-            chmod +x ./{script}.sh 
-            ./{script}.sh
-            popd
-        """        
-        data["resources"][i]["properties"]["metadata"]["items"][0]["value"] = startup
-
-    write_yaml(instance, data)
-    return
-
-def deploy():
-    if not os.path.exists(CONFIG):
-        print(f"Template file doesn't exist: {CONFIG}!")
-        sys.exit(1)
-
-    command = f"{DEPLOY} {CONFIG}"
+def deploy(stack="duclos-dev"):
+    config = template()
+    exit(0)
+    command = f"gcloud deployment-manager -q deployments create {stack} --config {config}"
     subprocess.run(command, shell=True)
     return
 
-def delete():
-    command = f"{DELETE}"
+def delete(stack="duclos-dev"):
+    command = f"gcloud deployment-manager -q deployments delete {stack}"
     subprocess.run(command, shell=True)
     return
 
 def parse():
-    arg_def = argparse.ArgumentParser()
+    arg_def = argparse.ArgumentParser(
+        description='Script to automate GCP stack launch.',
+        epilog='Example: main.py --action/-a pull -p <prefix>'
+    )
+
     arg_def.add_argument(
-        "action",
-        choices=["template", "deploy", "delete", "package", "scripts"],
-        help="Example: python3 main.py <action>"
+        "-a", "--action",
+        type=str,
+        choices=["deploy", "delete"],
+        dest="action",
     )
 
     return arg_def.parse_args()
@@ -96,10 +46,6 @@ def main():
     args = parse()
 
     match args.action:
-        case "template":
-            template()
-        case "package":
-            pkg()
         case "deploy":
             deploy()
         case "delete":
