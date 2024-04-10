54b300beca1331dd635e44a10fac527ac889109e
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-04-10 18:43:08 -0400

    Relocated message.proto

--- cmake/proto.cmake
+++ cmake/proto.cmake
@@ -1,5 +1,3 @@
 set(PROTO_BIN   protobin)
-set(PROTO_FILES ${PROJECT_SOURCE_DIR}/lib/proto/message.proto)
-set(PROTO_LIBRARY_DIR ${PROJECT_SOURCE_DIR}/lib/proto)
-
+set(PROTO_FILES ${PROJECT_SOURCE_DIR}/src/proto/message.proto)
 PROTOBUF_GENERATE_CPP(PROTO_SRCS PROTO_HDRS ${PROTO_FILES})
