7c8033ebd39f329650bd1348df9e77e0afc28e37
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-04-18 15:32:42 -0400

    Updated examples as self-contained makefile projects

--- cmake/examples.cmake
+++
@@ -1,8 +0,0 @@
-set(EXAMPLES_DIR ${PROJECT_SOURCE_DIR}/examples)
-file(GLOB DIRS RELATIVE ${EXAMPLES_DIR} ${EXAMPLES_DIR}/*)
-
-foreach(NAME IN LISTS DIRS)
-    if(IS_DIRECTORY ${EXAMPLES_DIR}/${NAME})
-        add_subdirectory("${EXAMPLES_DIR}/${NAME}/")
-    endif()
-endforeach()
