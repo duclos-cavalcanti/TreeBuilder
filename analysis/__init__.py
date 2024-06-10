28d3b62c3a05dee0ab5da4167041a4b4635dd668
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-06-10 13:12:17 -0400

    Implemented barebones analysis module

---
+++ analysis/__init__.py
@@ -0,0 +1,6 @@
+from .core import main as run
+
+__all__ = [
+    'run'
+]
+
--- analysis/core.py
+++ analysis/core.py
@@ -4,42 +4,6 @@ import os
 import shutil
 import tarfile
 import csv
-import json
-
-import boto3
-
-import ipdb
-# ipdb.set_trace()
-
-class MsgData():
-    def __init__(self, arr:list):
-        if len(arr) != 6:
-            raise RuntimeError("Receiver Data has wrong format!")
-
-        self.id         = int(arr[0])
-        self.latency    = int(arr[1])
-        self.recvid     = int(arr[2])
-        self.holding    = int(arr[3])
-        self.release    = int(arr[4])
-        self.deadline   = int(arr[5])
-
-class ReceiverData():
-    def read_csv(self, f:str):
-        arr = []
-        with open(f, newline='') as file:
-            reader = csv.reader(file)
-            for _, row in enumerate(reader):
-                if len(row) > 0 and not ("HEADER" in row[0]):
-                    arr.append(MsgData(row))
-
-            return arr
-
-    def __init__(self, f:str, n:int):
-        # print(f"Reading: {f}")
-        self.data = self.read_csv(f)
-        self.id = self.data[0].recvid
-        self.n = n
-
 
 def run(command:str, verbose:bool=False) -> list[str]:
     ret = subprocess.run(command.split(" "), stdout=subprocess.PIPE, text=True)
@@ -50,37 +14,22 @@ def run(command:str, verbose:bool=False) -> list[str]:
 
     return arr
 
-def pull(prefix:str, cloud:str="gcp"):
-    print(f"PULLING: {prefix}")
-    def awspull(prefix:str):
-        s3 = boto3.client('s3')
-        objs = s3.list_objects(Bucket="expresults", Prefix=prefix).get('Contents', [])
-        if len(objs) <= 1: 
-            raise RuntimeError(f"No bucket found on AWS matching prefix: {prefix}")
-        else:
-            dir = objs[0]['Key'].split("/")[0]
-            if os.path.isdir(os.path.join(os.getcwd(), "assets", dir)):
-                print(f"Bucket {dir} already pulled down!")
-            else:
-                os.makedirs(f"assets/{dir}", exist_ok=True)
-                for o in objs: 
-                    k = o['Key']
-                    path = f"assets/{o['Key']}"
-                    s3.download_file("expresults", k, path)
-                    print(f"Downloaded: {path}")
+# treefinder-06-10-14:21:46
 
-            return dir
-        
+def pull(args):
+    print(f"PULLING: {args.prefix}")
     def gpull(prefix:str):
         arr = run("gcloud storage ls gs://exp-results-nyu-systems-multicast/")
         for a in arr:
             if prefix in a:
                 dir = a.split("/")[-2]
-                if os.path.isdir(os.path.join(os.getcwd(), "assets", dir)):
+                root = os.path.join(os.getcwd(), "analysis", "data")
+                path = os.path.join(os.getcwd(), "analysis", "data", dir)
+                if os.path.isdir(path):
                     print(f"Bucket {dir} already pulled down!")
                 else:
-                    run(f"gcloud storage cp --recursive gs://exp-results-nyu-systems-multicast/{dir}* assets")
-                return dir
+                    run(f"gcloud storage cp --recursive gs://exp-results-nyu-systems-multicast/{dir}* {root}")
+                return path
         raise RuntimeError(f"No bucket found on GCP matching prefix: {prefix}")
 
 
@@ -91,200 +40,48 @@ def pull(prefix:str, cloud:str="gcp"):
                     tar.extractall(path=src)
                     print(f"Decompressed: {f}")
 
-    if cloud == "gcp":
-        dir = gpull(prefix)
-    else:
-        dir = awspull(prefix)
+    dir = gpull(args.prefix)
+    extract(f"{dir}")
 
-    extract(f"./assets/{dir}")
+def process(args):
+    raise NotImplementedError()
 
-def process(prefix:str):
-    print(f"PROCESSING: {prefix}")
-    def getdir(src:str, match:str):
-        for f in os.listdir(src):
-            dir = os.path.join(src, f)
-            if os.path.isdir(dir):
-                if match in f:
-                    return dir
-        raise RuntimeError(f"No directory found matching prefix: {prefix}")
-
-    def getfiles(src:str):
-        arr = []
-        for f in os.listdir(src):
-            file = os.path.join(src, f)
-            if (file.endswith(".csv")):
-                arr.append(file)
-
-        return arr
-
-    def summarize(rdata:ReceiverData, recvs:dict, msgs:dict):
-        n = len(rdata.data)
-        cnt = 0
-
-        for msgdata in rdata.data:
-            if not (msgdata.id in msgs.keys()):
-                msgs[msgdata.id] = {
-                    "unfair_percentage": 0,
-                    "unfair": 0,
-                    "total": rdata.n,
-                }
-
-            # unfair delivery
-            if msgdata.holding == 0:
-                cnt += 1
-                msgs[msgdata.id]["unfair"] += 1
-                msgs[msgdata.id]["unfair_percentage"] = ( 
-                    (msgs[msgdata.id]["unfair"]
-                     /
-                     msgs[msgdata.id]["total"]) * 100 
-                )
-
-        recvs[rdata.id] = {
-                "unfair_percentage": ( (cnt/n) * 100 ),
-                "unfair": cnt,
-                "total": n,
-                "id": rdata.id
-        }
-
-    def write(name:str, data:dict):
-        with open(f"{name}.json", "w") as f:
-            json.dump(data, f)
-
-    dir = getdir("assets", prefix)
-    files = getfiles(dir)
-
-    recvs = {}
-    msgs = {}
-
-    n = len(files)
-    for f in files: 
-        rdata = ReceiverData(f, n)
-        summarize(rdata, recvs, msgs)
-        print(f"Processed: {f}")
-
-    ret = {
-        "receivers": recvs,
-        "msgs": msgs,
-    }
-
-    write(prefix,ret)
-
-def read(prefix:str):
-    print(f"READING: {prefix}")
-    def sort(d:dict, i:int, s:str):
-        return sorted(d.items(), key=lambda x: x[i][s], reverse=True)
-
-    with open(f"{prefix}.json", "r") as f:
-        data = json.load(f)
-
-        recvs = data["receivers"]
-        msgs = data["msgs"]
-
-        # for r, i in recvs.items():
-        #     print(i)
-
-        cnt = 0
-        total = len(msgs)
-        for _, i in msgs.items():
-            if i["unfair"] > 0: 
-                cnt += 1
-        
-        print(f"Unfair Messages: {(cnt/total) * 100}% | {cnt}/{total}")
-        for r, i in sort(recvs, 1, "unfair_percentage"):
-            print(f"Receiver {r}: Percentage = {i['unfair_percentage']}%")
-
-def compare(prefix:str, command="diff"):
-    def getdir(src:str, match:str):
-        for f in os.listdir(src):
-            dir = os.path.join(src, f)
-            if os.path.isdir(dir):
-                if match in f:
-                    return dir
-        raise RuntimeError(f"No directory found matching prefix: {match}")
-
-    arr = prefix.split("/")
-    p1 = arr[0]
-    p2 = arr[1]
-    dir1 = getdir("assets", p1)
-    dir2 = getdir("assets", p2)
-    os.system(f"{command} {dir1}/config.h {dir2}/config.h")
-
-def copy(prefix:str):
-    print(f"COPYING: {prefix}")
-    def getdir(src:str, match:str):
-        for f in os.listdir(src):
-            dir = os.path.join(src, f)
-            if os.path.isdir(dir):
-                if match in f:
-                    return dir
-        raise RuntimeError(f"No directory found matching prefix: {prefix}")
-
-
-    os.system("rm -fv assets/*.csv*")
-    dir = getdir("assets", prefix)
-    for f in os.listdir(dir):
-        if f.endswith("tar.gz"):
-            src = os.path.join(dir, f)
-            dst = os.path.join("assets", f)
-            print(f"Copied: {shutil.copy(src, dst)}")
-
-
-def parse():
-    parser = argparse.ArgumentParser(
-        description='Script to automate GCP data analysis.',
-        epilog='Example: main.py --mode/-m pull -p <prefix>'
+def parse(rem=None):
+    arg_def = argparse.ArgumentParser(
+        description='Script to automate data analysis.',
+        epilog='Example: main.py --action/-a pull -p <prefix>'
     )
 
-    parser.add_argument(
-        "-m", "--mode",
+    arg_def.add_argument(
+        "-a", "--action",
         type=str,
-        choices=["pull", "process", "read", "full", "copy", "compare"],
+        choices=["pull", "process"],
         required=True,
-        dest="mode",
+        dest="action",
     )
 
-    parser.add_argument(
-        "-c", "--cloud",
-        type=str,
-        choices=["gcp", "aws"],
-        required=False,
-        default="gcp",
-        dest="cloud",
-    )
-
-    parser.add_argument(
+    arg_def.add_argument(
         "-p", "--prefix",
         type=str,
         required=True,
         dest="prefix",
     )
 
-    args = parser.parse_args()
+    if not rem: args = arg_def.parse_args()
+    else: args = arg_def.parse_args(rem)
+
     return args
 
+def main(rem):
+    args = parse(rem)
+
+    match args.action:
+        case "pull":    pull(args)
+        case "process": process(args)
+        case _:         raise NotImplementedError()
+
+    return
+
+
 if __name__ == "__main__":
-    args = parse()
-
-    if args.mode == "pull": 
-        pull(args.prefix, args.cloud)
-
-    elif args.mode == "process": 
-        process(args.prefix)
-
-    elif args.mode == "read": 
-        read(args.prefix)
-
-    elif args.mode == "compare": 
-        compare(args.prefix)
-
-    elif args.mode == "full": 
-        pull(args.prefix, args.cloud)
-        process(args.prefix)
-        read(args.prefix)
-
-    elif args.mode == "copy": 
-        copy(args.prefix)
-
-    else:
-        raise NotImplementedError(f"Mode: {args.mode} not implemented!")
-
+    main(rem=None)
