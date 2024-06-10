2440e7952b9229c6e7438e73cf2c1bbddb0c43ff
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-06-10 13:13:45 -0400

    Implemented barebones deploy module

--- deploy/core.py
+++ deploy/core.py
@@ -1,54 +1,151 @@
 import os
 import argparse
+import yaml
 
-# import shutil
-# import ipdb
-# ipdb.set_trace()
+from .utils import *
 
-from .utils import execute, lexecute
+def compress(dst:str):
+    command = "tar --exclude=jasper \
+		       --exclude=project.tar.gz \
+		       --exclude=.git \
+		       --exclude=.gitkeep \
+		       --exclude=.gitignore \
+		       --exclude=.gitmodules \
+		       --exclude=examples \
+		       --exclude=lib \
+		       --exclude=build \
+		       --exclude=.cache \
+		       --exclude=terra \
+		       --exclude=infra \
+		       --exclude=analysis \
+		       --exclude-vcs-ignores \
+		       -zcvf"
 
-def deploy(args):
-    infra = args.infra
-    wdir = os.path.join(os.getcwd(), "deploy", "terra", infra)
+    command = f"{command} {dst}/project.tar.gz ."
+    execute(command)
+
+def config(args):
+    config = "plans/default.yaml"
+    port   = 9091
+    addrs  = [ f"10.1.1.{i}:{port}" for i in range(args.pool + 1) ]
+    data = {
+            "infra": args.infra,
+            "addrs": addrs,
+            "params": {
+                "hyperparameter":   args.fanout * 2,
+                "rate":             args.rate,
+                "duration":         args.duration,
+                "fanout":           args.fanout,
+                "depth":            args.depth,
+            }, 
+            "runs": [ 
+                {
+                    "name": "BEST",
+                    "strategy": {
+                        "best": True
+                    }
+                },
+                {
+                    "name": "WORST",
+                    "strategy": {
+                        "best": False
+                    }
+                },
+                {
+                    "name": "RAND",
+                    "strategy": {
+                        "best": True
+                    },
+                }
+            ],
+    }
+
+    with open(config, "w") as file: 
+        yaml.dump(data, file, sort_keys=False)
+
+def plan(args):
+    wdir = os.path.join(os.getcwd(), "infra", "terra", args.infra)
     if not os.path.isdir(wdir): raise RuntimeError(f"Not a directory: {wdir}")
 
-    lexecute(f"terraform init", wdir=wdir, verbose=True)
-    os.system(f"cd {wdir} && terraform apply -auto-approve")
+    compress(f"{wdir}/extract")
+    out = try_execute(f"terraform init", wdir=wdir)
+    out = try_execute(f"terraform plan -out=tf.plan -var mode=default -var pool={args.pool}", wdir=wdir)
+    with open(f"{wdir}/plan.log", "w") as file: file.write(out.decode("utf-8"))
+    config(args)
 
-    return
+def deploy(args):
+    wdir = os.path.join(os.getcwd(), "infra", "terra", args.infra)
+    if not os.path.isdir(wdir): raise RuntimeError(f"Not a directory: {wdir}")
 
-def delete(args):
-    return
+    lexecute(f"terraform apply tf.plan", wdir=wdir)
+
+def destroy(args):
+    wdir = os.path.join(os.getcwd(), "infra", "terra", args.infra)
+    if not os.path.isdir(wdir): raise RuntimeError(f"Not a directory: {wdir}")
+
+    lexecute(f"terraform destroy -auto-approve", wdir=wdir)
 
 def parse(rem=None):
     arg_def = argparse.ArgumentParser(
         description='Module to automate terraform stack management.',
-        epilog='Example: core.py -a create -m docker'
+        epilog='Example: core.py -a plan -i gcp'
     )
 
     arg_def.add_argument(
         "-a", "--action",
         type=str,
-        required=False,
-        default="deploy",
-        choices=["build", "deploy", "delete"],
+        required=True,
+        choices=["plan", "deploy", "destroy"],
         dest="action",
     )
 
     arg_def.add_argument(
         "-i", "--infra",
         type=str,
-        required=True,
+        required=False,
+        default="gcp",
         choices=["docker", "gcp"],
         dest="infra",
     )
 
     arg_def.add_argument(
-        "-n", "--name",
-        type=str,
-        default="test",
+        "-f", "--fanout",
+        type=int,
+        default=2,
         required=False,
-        dest="name",
+        dest="fanout",
+    )
+
+    arg_def.add_argument(
+        "-d", "--depth",
+        type=int,
+        default=2,
+        required=False,
+        dest="depth",
+    )
+
+    arg_def.add_argument(
+        "-r", "--rate",
+        type=int,
+        default=10,
+        required=False,
+        dest="rate",
+    )
+
+    arg_def.add_argument(
+        "-t", "--time",
+        type=int,
+        default=10,
+        required=False,
+        dest="duration",
+    )
+
+    arg_def.add_argument(
+        "-p", "--pool",
+        type=int,
+        default=9,
+        required=False,
+        dest="pool",
     )
 
     if not rem: args = arg_def.parse_args()
@@ -60,11 +157,12 @@ def main(rem):
     args = parse(rem)
 
     match args.action:
+        case "plan":    plan(args)
         case "deploy":  deploy(args)
-        case "delete":  delete(args)
+        case "destroy": destroy(args)
+        case _:         raise NotImplementedError()
 
     return
-
 
 if __name__ == "__main__":
     main(rem=None)
--- deploy/utils.py
+++ deploy/utils.py
@@ -1,60 +1,59 @@
-import sys
 import os
-import re
 import subprocess
-import json
-# import hcl
-
-def get_ts(f:str):
-    if not os.path.isfile(f):
-        raise RuntimeError(f"Not a file: {f}")
-    
-    ts = os.path.getmtime(f)
-    return ts
-
-# def read_hcl(f:str):
-#     data = None
-#     with open(f, 'r') as fp: data = hcl.load(fp)
-#     return data
-# 
-# def write_hcl(f:str, data):
-#     with open(f, 'w') as fp: 
-#         json.dump(data, fp)
-
-
-def lexecute(command:str, wdir=None, verbose=False):
-    command = re.sub(r'[\s\n]+', ' ', command)
-    p = subprocess.Popen(command.split(), 
-                         stdout=subprocess.PIPE, 
-                         stderr=subprocess.PIPE, 
-                         cwd=wdir)
-                         
-    if verbose:
-        print(f"{command}")
-        for line in p.stdout: 
-            print(line.decode('utf-8'), end='')
-
-    p.wait()
-
-    if p.returncode != 0: 
-        err = p.stderr.read().decode('utf-8')
-        print(err, file=sys.stderr)
-        raise RuntimeError(f"Error[{p.returncode}]: {command}")
-
-def execute(command:str, wdir=None, verbose=False):
-    ret, out, err = try_execute(command, wdir)
-    if ret == 0:
-        if verbose: 
-            print(out.decode('utf-8'))
-        return
-    else:
-        print(f"COMMAND FAILED: '{command}'")
-        print(f"STDOUT:")
-        print(f"{out.decode('utf-8')}")
-        exit(ret)
 
 def try_execute(command:str, wdir=None):
+    ret = 0
+    out = ""
+    err = ""
+
+    try: 
+        ret, out, err = execute(command, wdir)
+        return out
+
+    except Exception as e:
+        print("OUTPUT: \n{out}")
+        raise(e)
+
+def lexecute(command:str, wdir=None):
     if not wdir: wdir = os.getcwd()
+
+    ret = 0
+    out = "" 
+    err = ""
+
+    try:
+        arr = command.split()
+        p = subprocess.Popen(arr, 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE, 
+                            cwd=wdir)
+                         
+        print(f"{command}")
+        for line in p.stdout: print(line.decode('utf-8'), end='')
+
+        p.wait()
+
+        ret = p.returncode
+        out = p.stdout.read().decode('utf-8')
+        err = p.stderr.read().decode('utf-8')
+
+    except Exception as e:
+        raise(e)
+
+    finally: 
+        if ret != 0:
+            raise RuntimeError()
+
+    return ret, out, err
+
+
+def execute(command:str, wdir=None):
+    if not wdir: wdir = os.getcwd()
+
+    ret = 0
+    out = ""
+    err = ""
+
     try:
         arr = command.split()
         p = subprocess.Popen(arr, 
@@ -63,7 +62,12 @@ def try_execute(command:str, wdir=None):
                              cwd=wdir)
         out, err = p.communicate()
         ret = p.returncode
+
     except Exception as e:
         raise(e)
+
+    finally: 
+        if ret != 0:
+            raise RuntimeError()
 
     return ret, out, err
