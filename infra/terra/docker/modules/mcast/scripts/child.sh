1ad03afeba7c000665fda215943efdd5fde6380d
Daniel Duclos-Cavalcanti <me@duclos.dev>
2024-06-14 18:06:19 -0400

    Added separate egress and ingress delay functions

--- infra/terra/docker/modules/mcast/scripts/child.sh
+++ infra/terra/docker/modules/mcast/scripts/child.sh
@@ -8,25 +8,19 @@ command="$@"
 export ROLE="$role"
 
 edelay() {
-    if [ $count -gt 0 ] && (( $count % 2 == 0)); then 
-        dur=300
-        sudo tc qdisc add dev "eth0" root netem delay ${dur}ms
-        echo "SUCCESSFULL EGRESS DELAY: ${dur}ms"
-    fi
+    dur="$1"
+    sudo tc qdisc add dev "eth0" root netem delay ${dur}ms
+    echo "SUCCESSFULL EGRESS DELAY: ${dur}ms"
 }
 
 idelay() {
-    if [ $count -gt 0 ] && (( $count % 2 == 0)); then 
-        dur=300
-        sudo modprobe ifb 
-        sudo modprobe sch_netem
-        sudo ip link add ifb0 type ifb
-        sudo ip link set ifb0 up
-        sudo tc qdisc add dev eth0 handle ffff: ingress
-        sudo tc filter add dev eth0 parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
-        sudo tc qdisc add dev ifb0 root netem delay ${dur}ms
-        echo "SUCCESSFULL INGRESS DELAY: ${dur}ms"
-    fi
+    dur="$1"
+    sudo ip link add ifb0 type ifb
+    sudo ip link set ifb0 up
+    sudo tc qdisc add dev eth0 handle ffff: ingress
+    sudo tc filter add dev eth0 parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
+    sudo tc qdisc add dev ifb0 root netem delay ${dur}ms
+    echo "SUCCESSFULL INGRESS DELAY: ${dur}ms"
 }
 
 TAR="/work/project.tar.gz"
@@ -39,7 +33,10 @@ pushd /work/project/build
     cmake ..
     make
     pushd /work/project/
-        delay
+        if [ $count -gt 0 ] && (( $count % 2 == 0)); then 
+            idelay 10
+        fi
+
         command="${command} -v"
         echo ${command}
         ${command}
